64afc110b977d35a72a8703c6effa6752c498373
Joe Nelson <cred+github@begriffs.com>
2013-06-07 13:42:26 -0500

    Add some sassy backtalk

--- talk.rb
+++ talk.rb
@@ -1,14 +1,29 @@
 require 'sinatra'
 require 'builder'
+require 'cleverbot'
 
 enable :sessions
 set :session_secret, ENV['SESSION_KEY']
 
 post '/' do
+  session[:chat_context] = Marshal.dump Hash.new
   builder do |xml|
     xml.instruct!
     xml.Response do
-      xml.Say("Hello world")
+      xml.Say "Hello, I love you. Talk to me. (I'll respond faster if you press pound after speaking.)"
+      xml.Record(maxLength: 10, finishOnKey: '#', transcribeCallback: '/reply', playBeep: false)
+    end
+  end
+end
+
+post '/reply' do
+  context = Cleverbot::Client.write params[:TranscriptionText], Marshal.load(session[:chat_context])
+  session[:chat_context] = Marshal.dump context
+  builder do |xml|
+    xml.instruct!
+    xml.Response do
+      xml.Say context[:message]
+      xml.Record(maxLength: 10, finishOnKey: '#', transcribeCallback: '.', playBeep: false)
     end
   end
 end
