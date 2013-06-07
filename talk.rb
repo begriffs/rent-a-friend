154142ac504a51eacab7f8caf5527b31b909bd1e
Joe Nelson <cred+github@begriffs.com>
2013-06-07 17:34:34 -0500

    Use an interrupt for synchronous transcoding

--- talk.rb
+++ talk.rb
@@ -1,31 +1,41 @@
 require 'sinatra'
-require 'builder'
+require 'twilio-ruby'
 require 'cleverbot'
 
-enable :sessions
-set :session_secret, ENV['SESSION_KEY']
+use Rack::Session::Cookie, :path => '/', :expire_after => 2592000, :secret => ENV['SESSION_KEY']
+
+set :public_folder, 'public'
 
 post '/' do
-  session[:chat_context] = Marshal.dump Hash.new
-  builder do |xml|
-    xml.instruct!
-    xml.Response do
-      xml.Say "Hello, I love you. Talk to me. (I'll respond faster if you press pound after speaking.)"
-      xml.Record(maxLength: 10, finishOnKey: '#', transcribeCallback: '/reply', playBeep: false)
-    end
+  session['ChatContext'] ||= Marshal.dump Hash.new
+
+  if session['TranscriptionText']
+    context = Cleverbot::Client.write(
+      session['TranscriptionText'],
+      Marshal.load(session['ChatContext'])
+    )
+    session['ChatContext']       = Marshal.dump context
+    session['TranscriptionText'] = ''
+    response = context['message']
+  else
+    response = "Hello, I love you. Talk to me (I'll respond faster if you press pound after speaking)."
   end
+
+  Twilio::TwiML::Response.new do |r|
+    r.Say response
+    r.Record action: '/wait', maxLength: 10, finishOnKey: '#', transcribeCallback: '/transcribed', playBeep: false
+  end.text
 end
 
-post '/reply' do
-  puts session[:chat_context].to_s
-  context = Cleverbot::Client.write params[:TranscriptionText], Marshal.load(session[:chat_context] || "\x04\b{\x00")
-  puts context
-  session[:chat_context] = Marshal.dump context
-  builder do |xml|
-    xml.instruct!
-    xml.Response do
-      xml.Say context['message']
-      xml.Record(maxLength: 10, finishOnKey: '#', transcribeCallback: '/reply', playBeep: false)
-    end
-  end
+post '/wait' do
+  Twilio::TwiML::Response.new do |r|
+    r.Play(request.base_url + '/wait.mp3', loop: 100)
+  end.text
+end
+
+post '/transcribed' do
+  session['TranscriptionText'] = params['TranscriptionText']
+
+  client = Twilio::REST::Client.new params['AccountSid'], ENV['TWILIO_AUTH']
+  client.account.calls.get(params['CallSid']).redirect_to request.base_url
 end
