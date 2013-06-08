f4601ee6b4276c9490325725a9d563fce1db0f14
Joe Nelson <cred+github@begriffs.com>
2013-06-08 11:18:41 -0500

    Get cleverbot response before terminating hold-music

--- talk.rb
+++ talk.rb
@@ -8,21 +8,8 @@ set :cache, Dalli::Client.new
 set :public_folder, 'public'
 
 post '/' do
-  call_sid      = params['CallSid']
-  context       = Marshal.load(settings.cache.get(call_sid + 'chat') || Marshal.dump(Hash.new))
-  transcription = settings.cache.get(call_sid + 'transcription')
-
-  if transcription
-    context = Cleverbot::Client.write(transcription, context)
-    settings.cache.set(call_sid + 'chat', Marshal.dump(context))
-    settings.cache.set(call_sid + 'transcription', nil)
-    response = context['message']
-  else
-    response = "Hello, I love you. Talk to me."
-  end
-
   Twilio::TwiML::Response.new do |r|
-    r.Say response
+    r.Say settings.cache.get(params['CallSid'] + 'reply') || "Hello, I love you. Talk to me."
     r.Record action: '/wait', maxLength: 10, timeout: 3, finishOnKey: '#', transcribeCallback: '/transcribed', playBeep: false
   end.text
 end
@@ -34,7 +21,14 @@ post '/wait' do
 end
 
 post '/transcribed' do
-  settings.cache.set(params['CallSid'] + 'transcription', params['TranscriptionText'])
+  call_sid = params['CallSid']
+  context  = Cleverbot::Client.write(
+    params['TranscriptionText'],
+    Marshal.load(settings.cache.get(call_sid + 'chat') || Marshal.dump(Hash.new))
+  )
+
+  settings.cache.set(call_sid + 'chat', Marshal.dump(context))
+  settings.cache.set(call_sid + 'reply', context['message'])
 
   client = Twilio::REST::Client.new params['AccountSid'], ENV['TWILIO_AUTH']
   client.account.calls.get(params['CallSid']).redirect_to request.base_url
